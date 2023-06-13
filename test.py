7a38a0ec3795b3b0b9963f4768525302b69e800b
Newbern <91991219+Newbern@users.noreply.github.com>
2023-06-13 11:53:38 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,12 @@ from playsound import playsound
 import multiprocessing
 from time import sleep
 
-
-# song = "Music\Rock\Favorite\Creeps.mp3"
-
-
 def play():
-    playsound("Music\Rock\Favorite\Creeps.mp3")
+    playsound("Music.mp3")
 if __name__ == "__main__":
     M1 = multiprocessing.Process(target=play)
 
     M1.start()
     sleep(0.1)
-    input(">>>")
-
+    input("Press key to End Music...")
     M1.terminate()
