01f7d29822e39d063e506b6bec7794497bdd2a03
Newbern <91991219+Newbern@users.noreply.github.com>
2023-06-26 13:46:44 -0500

    Player.py

--- Player.py
+++ Player.py
@@ -5,8 +5,7 @@ from time import sleep
 def play():
     playsound("Music.mp3")
 if __name__ == "__main__":
-    M1 = multiprocessing.Process(target=play)
-
+    M1 = multiprocessing.Process(target=play)
     M1.start()
     sleep(0.1)
     input("Press key to End Music...")
